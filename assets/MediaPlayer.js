956f3af2711bc92aab34b1d83161ced6904214f8
Enrique Chalco <enrique.chalco@unmsm.edu.pe>
2022-02-07 11:38:00 -0500

    el primer plugin-autoplay

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,5 +1,14 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins =config.plugins || [];
+
+    this._initPlugins();
+  }
+
+  MediaPlayer.prototype._initPlugins = function(){
+    this.plugins.forEach(plugin => {
+      plugin.run(this);
+    });
   }
 
   MediaPlayer.prototype.play = function() {
@@ -17,4 +26,11 @@ function MediaPlayer(config) {
       this.pause();
     }
   };
+
+  MediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
+  }
+  MediaPlayer.prototype.unmute = function(){
+    this.media.muted =false;
+  }
 export default MediaPlayer;
